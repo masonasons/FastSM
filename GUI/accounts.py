05bd5575b44c6bbcf59f1722017dffb7362ec0ca
Bri <briexists@gmail.com>
2026-01-13 23:34:43 -0600

    Add Remove account button to Accounts dialog
    - Adds confirmation dialog before removal
    - Stops streaming if active
    - Deletes account config folder
    - Switches to first account if current was removed
    - Prevents removing the last account

--- GUI/accounts.py
+++ GUI/accounts.py
@@ -1,7 +1,10 @@
 import application
 import wx
+import shutil
+import os
 from application import get_app
 from . import main, misc
+import speak
 
 class AccountsGui(wx.Dialog):
 	def __init__(self):
@@ -18,11 +21,13 @@ class AccountsGui(wx.Dialog):
 		self.load = wx.Button(self.panel, wx.ID_DEFAULT, "&Switch")
 		self.load.SetDefault()
 		self.load.Bind(wx.EVT_BUTTON, self.Load)
-#		self.load.Enable(False)
 		self.main_box.Add(self.load, 0, wx.ALL, 10)
 		self.new = wx.Button(self.panel, wx.ID_DEFAULT, "&Add account")
 		self.new.Bind(wx.EVT_BUTTON, self.New)
 		self.main_box.Add(self.new, 0, wx.ALL, 10)
+		self.remove = wx.Button(self.panel, -1, "&Remove account")
+		self.remove.Bind(wx.EVT_BUTTON, self.Remove)
+		self.main_box.Add(self.remove, 0, wx.ALL, 10)
 		self.close = wx.Button(self.panel, wx.ID_CANCEL, "&Cancel")
 		self.close.Bind(wx.EVT_BUTTON, self.OnClose)
 		self.main_box.Add(self.close, 0, wx.ALL, 10)
@@ -58,6 +63,66 @@ class AccountsGui(wx.Dialog):
 		main.window.on_list_change(None)
 		main.window.SetLabel(app.currentAccount.me.acct+" - "+application.name+" "+application.version)
 		self.Destroy()
+
+	def Remove(self, event):
+		app = get_app()
+		selection = self.list.GetSelection()
+		if selection < 0:
+			speak.speak("No account selected")
+			return
+
+		# Don't allow removing the last account
+		if len(app.accounts) <= 1:
+			speak.speak("Cannot remove the only account")
+			return
+
+		account_to_remove = app.accounts[selection]
+		account_name = account_to_remove.me.acct
+
+		# Confirm removal
+		dlg = wx.MessageDialog(self,
+			f"Are you sure you want to remove the account {account_name}? This will delete all local data for this account.",
+			"Confirm Removal",
+			wx.YES_NO | wx.NO_DEFAULT | wx.ICON_WARNING)
+		result = dlg.ShowModal()
+		dlg.Destroy()
+
+		if result != wx.ID_YES:
+			return
+
+		# Stop streaming if active
+		if hasattr(account_to_remove, 'stream') and account_to_remove.stream:
+			try:
+				account_to_remove.stream.close()
+			except:
+				pass
+
+		# Get the config path before removing
+		confpath = account_to_remove.confpath
+
+		# Remove from accounts list
+		app.accounts.remove(account_to_remove)
+		app.prefs.accounts -= 1
+
+		# Delete the account's config folder
+		if confpath and os.path.exists(confpath):
+			try:
+				shutil.rmtree(confpath)
+			except Exception as e:
+				speak.speak(f"Warning: Could not delete account data: {e}")
+
+		# If we removed the current account, switch to first account
+		if app.currentAccount == account_to_remove:
+			app.currentAccount = app.accounts[0]
+			main.window.refreshTimelines()
+			main.window.list.SetSelection(0)
+			main.window.on_list_change(None)
+			main.window.SetLabel(app.currentAccount.me.acct+" - "+application.name+" "+application.version)
+
+		# Refresh the list
+		self.list.Clear()
+		self.add_items()
+		speak.speak(f"Account {account_name} removed")
 
 	def OnClose(self, event):
 		self.Destroy()
