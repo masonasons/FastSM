1e62f2474988db40fe0db9f10c501682e936e856
Bryn <briexists@gmail.com>
2026-04-24 22:56:46 -0500

    Let the search dialog pick between posts and users (#23)
    The search timeline was hardcoded to result_type=statuses, so on
    instances without full-text indexing the regular Search dialog just
    returned nothing. User-search already worked via a separate menu
    entry, but most users never found it. Add a "Search for" choice to
    SearchGui so the same dialog dispatches to either misc.search (post
    timeline) or misc.user_search (user list) and the User Search menu
    shortcut still pre-selects "Users".
    
    Closes #23

--- GUI/search.py
+++ GUI/search.py
@@ -1,20 +1,39 @@
 import wx
 from . import misc, theme
 
+# Each entry: (label, internal-key). Keys map to misc.* dispatch below.
+_SEARCH_TYPES = [
+	("Posts", "posts"),
+	("Users", "users"),
+]
+
+
 class SearchGui(wx.Dialog):
-	def __init__(self,account, type="search"):
-		self.account=account
-		self.type=type
-		wx.Dialog.__init__(self, None, title="Search", size=(350,200))
+	def __init__(self, account, type="search"):
+		self.account = account
+		self.type = type
+		wx.Dialog.__init__(self, None, title="Search", size=(350, 200))
 		self.Bind(wx.EVT_CLOSE, self.OnClose)
 		self.panel = wx.Panel(self)
 		self.main_box = wx.BoxSizer(wx.VERTICAL)
 		self.text_label = wx.StaticText(self.panel, -1, "Search text")
 		self.main_box.Add(self.text_label, 0, wx.LEFT | wx.TOP, 10)
-		self.text = wx.TextCtrl(self.panel, -1, "",style=wx.TE_PROCESS_ENTER|wx.TE_DONTWRAP)
+		self.text = wx.TextCtrl(self.panel, -1, "", style=wx.TE_PROCESS_ENTER | wx.TE_DONTWRAP)
 		self.main_box.Add(self.text, 0, wx.EXPAND | wx.ALL, 10)
-		self.text.SetFocus()
 		self.text.Bind(wx.EVT_TEXT_ENTER, self.Search)
+
+		self.kind_label = wx.StaticText(self.panel, -1, "Search &for")
+		self.main_box.Add(self.kind_label, 0, wx.LEFT | wx.TOP, 10)
+		self.kind = wx.Choice(self.panel, -1, choices=[label for label, _ in _SEARCH_TYPES])
+		# Default to "Users" when the dialog was opened from the User Search
+		# menu entry; otherwise default to "Posts".
+		default_key = "users" if type == "user" else "posts"
+		for i, (_, key) in enumerate(_SEARCH_TYPES):
+			if key == default_key:
+				self.kind.SetSelection(i)
+				break
+		self.main_box.Add(self.kind, 0, wx.ALL, 10)
+
 		self.search = wx.Button(self.panel, wx.ID_DEFAULT, "&Search")
 		self.search.SetDefault()
 		self.search.Bind(wx.EVT_BUTTON, self.Search)
@@ -24,13 +43,18 @@ class SearchGui(wx.Dialog):
 		self.main_box.Add(self.close, 0, wx.ALL, 10)
 		self.panel.SetSizer(self.main_box)
 		self.panel.Layout()
+		self.text.SetFocus()
 		theme.apply_theme(self)
 
 	def Search(self, event):
-		if self.type=="search":
-			misc.search(self.account,self.text.GetValue())
+		query = self.text.GetValue().strip()
+		if not query:
+			return
+		key = _SEARCH_TYPES[self.kind.GetSelection()][1]
+		if key == "users":
+			misc.user_search(self.account, query)
 		else:
-			misc.user_search(self.account,self.text.GetValue())
+			misc.search(self.account, query)
 		self.Destroy()
 
 	def OnClose(self, event):
